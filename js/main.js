688ac3c1067a597e0cab4676daf0a28c068f3241
José Ángel Hernández Argüelles <zackangelft@gmail.com>
2026-04-29 19:10:23 -0600

    FuncionalidadCamara

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
-console.log("Arquitectura de Realidad Aumentada - UTL Semana 1");
+const video = document.getElementById('webcam');
+
+async function iniciarCamara() {
+    try {
+        const stream = await navigator.mediaDevices.getUserMedia({
+            video: { facingMode: { exact: "environment" } }
+        });
+        video.srcObject = stream;
+    } catch (err) {
+        console.error("No se pudo usar la cámara trasera, intentando cámara genérica:", err);
+        // Intento secundario si la cámara exacta falla
+        navigator.mediaDevices.getUserMedia({ video: true })
+            .then(stream => video.srcObject = stream);
+    }
+}
+
+iniciarCamara();
