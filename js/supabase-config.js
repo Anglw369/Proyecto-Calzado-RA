5d0c9a94fcbca617a9c25407aab2ed828e82f42c
José Ángel Hernández Argüelles <zackangelft@gmail.com>
2026-05-18 18:00:34 -0600

    subabse correcion

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -1,8 +1,7 @@
 // js/supabase-config.js
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 
-// PEGA AQUÍ TUS DATOS ENTRE LAS COMILLAS
-const supabaseUrl = 'AQUÍ_PEGA_LA_URL_DE_TU_PROJECT_URL'
-const supabaseKey = 'AQUÍ_PEGA_LA_LLAVE_ANON_PUBLIC'
+const supabaseUrl = 'https://lvzdvalxpywyqbiyvfqe.supabase.co'
+const supabaseKey = 'sb_publishable_H3ir5Ih3kBKsuX2nRaPFDQ_6Vecd855'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
